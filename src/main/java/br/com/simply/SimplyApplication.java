5a9b07e63324fc1380321489b2128915a6922a28
alexsouzajk <alexsouzaj83@gmail.com>
2020-09-22 17:08:31 -0300

    Criação dos arquivos do projeto

---
+++ src/main/java/br/com/simply/SimplyApplication.java
@@ -0,0 +1,13 @@
+package br.com.simply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyApplication.class, args);
+	}
+
+}
