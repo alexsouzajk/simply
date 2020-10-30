24d2a32465e5518bc1d640e7250dda4b02035415
alexsouzajk <alexsouzaj83@gmail.com>
2020-10-30 19:06:11 -0300

    Criação das telas

--- src/main/java/br/com/simply/request/LoginRequest.java
+++ src/main/java/br/com/simply/request/LoginRequest.java
@@ -12,4 +12,23 @@ public class LoginRequest {
 	private String login;
 	
 	private String senha;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
 }
